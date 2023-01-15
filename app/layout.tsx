e6b4d5e5bdbda3c4d12d3ef9717ba4a4b39d154a
thkim-mrt <taehyung.kim@myrealtrip.com>
2023-01-16 02:50:09 +0900

    레이아웃 설정중

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,39 @@
 "use client";
 
 import styled from '@emotion/styled';
+import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
 function layout({ children }: PropsWithChildren<unknown>): JSX.Element {
   return (
-    <HomeLayout>
-      <h2>홈 타이틀</h2>
-      {children}
-    </HomeLayout>
+    <MainLayout>
+      <HomeHeader>
+        <Link href="/list">리스트</Link>
+      </HomeHeader>
+      <MainWrapper>{children}</MainWrapper>
+    </MainLayout>
   )
 }
 
-const HomeLayout = styled.div`
+const MainLayout = styled.div`
+  padding: 100px 20px 0;
+`;
 
-`
+const HomeHeader = styled.header`
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  padding: 10px 60px;
+  background-color: #f2f2f2;
+`;
 
-export default layout
+const MainWrapper = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+export default layout;
